3e7b2577fa01ee3e49cea5eac31410e5def377ff
YoussefEhab2001 <youssef.ehab01@gmail.com>
2023-10-07 10:56:28 +0300

    Update main.c
    This is a test

--- main.c
+++ main.c
@@ -13,3 +13,12 @@ void printStars(int n){
 void mosama(){
 	printf("This is a test");
 }
+
+void arr_sum(int* Arr,size_arr)
+{
+	int sum =0;
+	for (int i =0 ; i<size_arr; i++)
+	{
+		sum += Arr[i];
+	}
+}
